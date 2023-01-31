943a50b971346a672bf061df416827dbbabb4fbd
Tihstep <tihstepmk@gmail.com>
2023-01-31 10:17:35 +0300

    build: Create sample generator sceleton

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import random
 import torch
 from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
 
 random.seed(0)
 
@@ -10,7 +11,7 @@ class UserItemDataDataset(Dataset):
 
         args:
             target_tensor: torch.Tensor, the corresponding action for <user, item> pair
-        """
+    """
     def __init__(self, data_tensor):
         self.data_tensor = data_tensor
 
@@ -21,3 +22,27 @@ class UserItemDataDataset(Dataset):
         return self.data_tensor.size()
 
 
+class SampleGenerator(object):
+    """Construct dataset for NCF
+
+            args:
+                data: pd.DataFrame, which contains 4 columns = ['userId', 'itemId', 'rating', 'timestamp']
+    """
+    def __init__(self, interactions):
+        assert 'userId' in interactions.columns
+        assert 'itemId' in interactions.columns
+        assert 'data' in interactions.columns
+
+        self.interactions = interactions
+        self.user_pool = set(self.interactions['userId'].unique())
+        self.item_pool = set(self.interactions['itemId'].unique())
+        # create negative item samples for NCF learning
+        #self.negatives = self._sample_negative(interactions)
+        #self.train_interactions, self.test_interactions = self._split_loo(self.interactions)
+
+    @staticmethod
+    def _split_loo(interactions):
+        pass
+
+    def _sample_negative(self, interactions):
+        pass
