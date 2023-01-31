7c6e6560470e02138a97f2cd6b030d44073a002f
Tihstep <tihstepmk@gmail.com>
2023-01-31 12:51:01 +0300

    feat: build train loop

---
+++ engine.py
@@ -0,0 +1,43 @@
+import torch
+from tensorboardX import SummaryWriter
+
+
+class Engine(object):
+    """Meta Engine for training & evaluating NCF model"""
+
+    def __init__(self, config):
+        self.config = config  # model configuration
+        self._writer = SummaryWriter(log_dir='runs/{}'.format(config['alias']))  # tensorboard writer
+        self._writer.add_text('config', str(config), 0)
+        self.opt = torch.optim.SGD(
+            self.model.parameters(),
+            lr=config['sgd_lr'],
+            momentum=config['sgd_momentum'],
+            weight_decay=config['l2_regularization']
+        )
+        self.crit = torch.nn.BCELoss()
+
+    def train_single_batch(self, users, items, ratings):
+        assert hasattr(self, 'model'), 'Please specify the exact model !'
+        if self.config['use_cuda'] is True:
+            users, items, ratings = users.cuda(), items.cuda(), ratings.cuda()
+        self.opt.zero_grad()
+        ratings_pred = self.model(users, items)
+        loss = self.crit(ratings_pred.view(-1), ratings)
+        loss.backward()
+        self.opt.step()
+        loss = loss.item()
+        return loss
+
+    def train_an_epoch(self, train_loader, epoch_id):
+        assert hasattr(self, 'model'), 'Please specify the exact model !'
+        self.model.train()
+        total_loss = 0
+        for batch_id, batch in enumerate(train_loader):
+            assert isinstance(batch[0], torch.LongTensor)
+            user, item, rating = batch[0], batch[1], batch[2]
+            rating = rating.float()
+            loss = self.train_single_batch(user, item, rating)
+            print('[Training Epoch {}] Batch {}, Loss {}'.format(epoch_id, batch_id, loss))
+            total_loss += loss
+        self._writer.add_scalar('model/loss', total_loss, epoch_id)
