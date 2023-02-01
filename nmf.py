deb90bb4f51c1625663ed52e8c299323ca0472d0
Tihstep <tihstepmk@gmail.com>
2023-02-01 15:19:30 +0300

    Create nmf initilization

--- nmf.py
+++ nmf.py
@@ -1,9 +1,30 @@
 import torch
 from gmf import GMF
 from mlp import MLP
+from engine import Engine
 
 
 class NeuMF(torch.nn.Module):
     def __init__(self, config):
-        pass
+        super(NeuMF, self).__init__()
+        self.config = config
+        self.num_users = config['num_users']
+        self.num_items = config['num_items']
+        self.hidden_dim_mf = config['hidden_dim_mf']
+        self.hidden_dim_mlp = config['hidden_dim_mlp']
 
+        self.embedding_user_mlp = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.hidden_dim_mlp)
+        self.embedding_item_mlp = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.hidden_dim_mlp)
+        self.embedding_user_mf = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.hidden_dim_mf)
+        self.embedding_item_mf = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.hidden_dim_mf)
+
+        self.fc_layers = torch.nn.ModuleList()
+        for idx, (in_size, out_size) in enumerate(zip(config['layers'][:-1], config['layers'][1:])):
+            self.fc_layers.append(torch.nn.Linear(in_size, out_size))
+
+        self.affine_output = torch.nn.Linear(in_features=config['layers'][-1] + config['hidden_dim_mf'], out_features=1)
+        self.logistic = torch.nn.Sigmoid()
+
+    def forward(self, user_indices, item_indices):
+        pass
+    
