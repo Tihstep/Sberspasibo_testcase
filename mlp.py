41a47ffd4ae431e0f125e3afa1095fffa81b2993
Tihstep <tihstepmk@gmail.com>
2023-02-01 10:47:26 +0300

    build: Create class MLP

--- mlp.py
+++ mlp.py
@@ -1,7 +1,24 @@
 import torch
+from gmf import GMF
+from engine import Engine
 
 
 class MLP(torch.nn.Module):
     def __init__(self, config):
-        pass
+        super(MLP, self).__init__()
+        self.config = config
+        self.num_users = config['num_users']
+        self.num_items = config['num_items']
+        self.hidden_dim = config['hidden_dim']
 
+        self.embedding_user = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.hidden_dim)
+        self.embedding_item = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.hidden_dim)
+
+        self.fc_layers = torch.nn.ModuleList()
+        for idx, (in_size, out_size) in enumerate(zip(config['layers'][:-1], config['layers'][1:])):
+            self.fc_layers.append(torch.nn.Linear(in_size, out_size))
+
+        self.affine_output = torch.nn.Linear(in_features=config['layers'][-1], out_features=1)
+        self.logistic = torch.nn.Sigmoid()
+    def forward(self):
+        pass
