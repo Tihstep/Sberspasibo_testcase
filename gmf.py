bd8b1eb4559b312e15097aa06e83338acf280d90
Tihstep <tihstepmk@gmail.com>
2023-02-01 06:53:46 +0300

    build: Create GMF class and forward pass

--- gmf.py
+++ gmf.py
@@ -1,15 +1,27 @@
 import torch
-
+from engine import Engine
 
 class GMF(torch.nn.Module):
     def __init__(self, config):
         super(GMF, self).__init__()
-        pass
-    def forward(self, user_indices, item_indices):
-        pass
+        self.num_users = config['num_users']
+        self.num_items = config['num_items']
+        self.hidden_dim = config['hidden_dim']
 
-    def init_weight(self):
-        pass
+        self.embedding_user = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.hidden_dim)
+        self.embedding_item = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.hidden_dim)
+
+        self.affine_output = torch.nn.Linear(in_features=self.latent_dim, out_features=1)
+        self.logistic = torch.nn.Sigmoid()
+
+    def forward(self, user_indices, item_indices):
+        user_embedding = self.embedding_user(user_indices)
+        item_embedding = self.embedding_item(item_indices)
+        element_product = torch.mul(user_embedding, item_embedding)
+        logits = self.affine_output(element_product)
+        interaction = self.logistic(logits)
+        return interaction
+
 
 
 class GMFEngine(Engine):
@@ -17,3 +29,4 @@ class GMFEngine(Engine):
 
     def __init__(self, config):
         pass
+    
